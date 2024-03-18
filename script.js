2123b11b1ac37c456c9d607148ad02a174e5f16b
cyber-mannu <sahanimannu844@gmail.com>
2024-03-18 15:22:17 +0530

    almost every things is fixed

--- script.js
+++ script.js
@@ -3,40 +3,45 @@ let colorFields = document.querySelector(".area");
 let rgb1, rgb2;
 
 const randomNum = () => {
-  hexCode = "0123456789abcdef";
+  let hexCode = "0123456789abcdef";
   let colors = "#";
   for (let i = 0; i < 6; i++) {
-    colors = colors + hexCode[Math.floor(Math.random() * 16)];
+    colors += hexCode[Math.floor(Math.random() * 16)];
   }
   return colors;
 };
 
-document.querySelector("#btn1").addEventListener("click", () => {
-  rgb1 = randomNum();
-  colorFields.innerHTML = `  background-image: linear-gradient(to right, 
-    ${rgb1}
-    , ${rgb2}`;
+const updateGradient = () => {
+  colorFields.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
+  colorFields.innerHTML = ` background-image :linear-gradient(to right, ${rgb1}, ${rgb2})`;
   document.body.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
-  document.querySelector("#btn1").innerHTML = `${rgb1}`;
   document.querySelector(
     "#btn1"
   ).style.backgroundImage = `linear-gradient(to right, ${rgb2}, ${rgb1})`;
+  document.querySelector(
+    "#btn2"
+  ).style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
+};
+
+document.querySelector("#btn1").addEventListener("click", () => {
+  rgb1 = randomNum();
+  updateGradient();
+  document.querySelector("#btn1").innerHTML = `${rgb1}`;
 });
 
 document.querySelector("#btn2").addEventListener("click", () => {
   rgb2 = randomNum();
-  colorFields.innerHTML = `  background-image: linear-gradient(to right, 
-  ${rgb1}
-  , ${rgb2}`;
-  document.body.style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
+  updateGradient();
   document.querySelector("#btn2").innerHTML = `${rgb2}`;
-  document.querySelector(
-    "#btn2"
-  ).style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
 });
 
 document.querySelector("#btn3").addEventListener("click", () => {
-  document.querySelector("#btn3").style.backgroundImage =`linear-gradient(to right, ${rgb2},  ${rgb1}, )`
+  rgb1 = randomNum();
+  rgb2 = randomNum();
+  updateGradient();
+  document.querySelector(
+    "#btn3"
+  ).style.backgroundImage = `linear-gradient(to right, ${rgb1}, ${rgb2})`;
 });
 
 colorFields.addEventListener("click", () => {
@@ -44,22 +49,7 @@ colorFields.addEventListener("click", () => {
   window.navigator.clipboard
     .writeText(hexaString)
     .then(() => {
-      alert("Color Copied Successfuly");
-      // done();
+      alert("Color Copied Successfully");
     })
-
     .catch(() => {});
 });
-
-function done() {
-  setTimeout(() => {
-    document.querySelector("#animebutton").addEventListener("click", () => {
-      gsap.from(".noticeboard h1", {
-        top: 30,
-        scale: 0,
-        y: -20,
-        opacity: 1,
-      });
-    });
-  }, 1000);
-}
